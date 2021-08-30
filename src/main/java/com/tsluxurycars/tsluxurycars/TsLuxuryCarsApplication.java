b28249d176977b7a352d9d434aae58e76cb06de0
Ethan Partin <ethan.partin@Zeus.attlocal.net>
2021-08-30 11:08:49 -0500

    Create test endpoint

---
+++ src/main/java/com/tsluxurycars/tsluxurycars/TsLuxuryCarsApplication.java
@@ -0,0 +1,13 @@
+package com.tsluxurycars.tsluxurycars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsLuxuryCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsLuxuryCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsluxurycars/tsluxurycars/TsLuxuryCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsluxurycars.tsluxurycars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsLuxuryCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
