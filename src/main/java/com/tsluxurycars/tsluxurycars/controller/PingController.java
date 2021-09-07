7f6986a69742e66bc2180f7f88d23eb65eaef56b
Ethan Partin <ethan.partin@Zeus-2.attlocal.net>
2021-09-07 14:28:26 -0500

    Make ping public

--- src/main/java/com/tsluxurycars/tsluxurycars/controller/PingController.java
+++ src/main/java/com/tsluxurycars/tsluxurycars/controller/PingController.java
@@ -1,13 +1,11 @@
 package com.tsluxurycars.tsluxurycars.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
-    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
         return ResponseEntity.ok("Pong!");
